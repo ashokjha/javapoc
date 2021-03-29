6e0e90420de22d08d9bba35929d22541a05dff87
Ashok Kumasr Jha <ashu.kr.jha@gmail.com>
2021-03-29 19:04:25 +0530

    Class loader

--- ashu.guide/src/main/java/Java/ashu/guide/Driver.java
+++ ashu.guide/src/main/java/Java/ashu/guide/Driver.java
@@ -1,17 +1,37 @@
 package Java.ashu.guide;
 
+import Java.ashu.guide.cl.ClassLoaderDemo;
+
 /**
  * Class loader
  *
  */
-public class ClassLoaderSample {
+public class Driver {
+	
 	public static void main(String[] args) {
+		Driver.pocClassLoader();
+	}
+	
+	
+	
+	
+	public static void pocClassLoader() {
+		
 		// classloader name of current class.
 		// Application/System classloader will load this class
 
-		Class c = ClassLoaderSample.class;
+		Class<ClassLoaderDemo> c = ClassLoaderDemo.class;
 
 		System.out.println(c.getClassLoader());
+		try {
+			ClassLoaderDemo obj = c.newInstance();
+			obj.hello();
+			
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		}
 
 		// If we print the classloader name of String, it will print null because it is
 		// an
---
+++ ashu.guide/src/main/java/Java/ashu/guide/cl/ClassLoaderDemo.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package Java.ashu.guide.cl;
+
+/**
+ * @author USER
+ *
+ */
+public class ClassLoaderDemo {
+	public void hello() {
+		System.out.println("IC");
+	}
+
+}
